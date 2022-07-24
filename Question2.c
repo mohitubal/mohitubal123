de08279ab7bc2831a126c21d674621590653eb6d
mohitubal <109760231+mohitubal@users.noreply.github.com>
2022-07-24 08:14:56 -0700

    Update Question2.c

--- Question2.c
+++ Question2.c
@@ -1,4 +1,4 @@
-//Write a program to print Hello in the first line and Students in the second line.
+//Q2.Write a program to print Hello in the first line and Students in the second line.
 
 #include<stdio.h>
 int main()
