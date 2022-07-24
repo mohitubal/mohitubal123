e4d29e0b2cb7d25fc2c96f597a433fbdab2dcff3
mohitubal <109760231+mohitubal@users.noreply.github.com>
2022-07-24 08:13:38 -0700

    Update Question11.c

--- Question11.c
+++ Question11.c
@@ -1,4 +1,4 @@
-//WAP to take time as an input in below given format and convert the time format and display the result as given below.
+//Q11.WAP to take time as an input in below given format and convert the time format and display the result as given below.
 
 #include<stdio.h>
 int main()
